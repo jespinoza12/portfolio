7fdf2d464c042bd4e3c8d748acfc6a21070b50fd
jespinoza12 <jespinoza@student.neumont.edu>
2023-05-15 22:58:21 -0600

    d

--- amplify/backend/function/portfolio56025c44/src/app.js
+++ amplify/backend/function/portfolio56025c44/src/app.js
@@ -1,40 +1,19 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-
-// declare a new express app
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express()
 app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
+app.use(cors());
+app.use(express.json());
 
-// Enable CORS for all methods
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "*")
   next()
 });
-
-
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
-
-// server used to send send emails
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
@@ -52,7 +31,7 @@ contactEmail.verify((error) => {
   }
 });
 
-router.post("/contact", (req, res) => {
+app.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
@@ -75,7 +54,8 @@ router.post("/contact", (req, res) => {
   });
 });
 
-
-app.listen(5000, () => console.log("Server Running"));
+app.listen(3000, function() {
+    console.log("App started")
+});
 
 module.exports = app
